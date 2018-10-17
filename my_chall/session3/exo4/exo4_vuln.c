47094205c6c77db10c53bf6e54f9e0d82fe5fdbb
meffre-q <dagger7x9@gmail.com>
2018-10-17 13:31:23 +0200

    ADD: Challenge

---
+++ my_chall/session3/exo4/exo4_vuln.c
@@ -0,0 +1,27 @@
+// gcc exo4_vuln.c -o exo4 -no-pie -fno-stack-protector -Wl,-z,relro,-z,now,-z,noexecstack
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main(void) {
+  int size = 0;
+  char buffer[128] = {0};
+
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stdout, NULL, _IONBF, 0);
+  memset(buffer, '\0', sizeof buffer);
+  puts("Can you please give me the size of your buffer:");
+  scanf("%d", &size);
+  if (size <= 0) {
+    puts("Newbie.");
+    exit(EXIT_SUCCESS);
+  }
+  puts("Can you please give me your buffer:");
+  read(0, buffer, size);
+  buffer[strcspn(buffer, "\n")]='\0';
+  printf("Thanks! Here is your buffer: %s", buffer);
+  return 0;
+
+}
